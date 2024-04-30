9f3dde22a704c63f48086378eafe73543d9886ea
MuhammadMahdiAmirpour <muhammadmahdiamirpour@gmail.com>
2024-04-30 23:18:57 +0330

    refactored date_regression.py file

--- src/data_cleaning/date_regression.py
+++ src/data_cleaning/date_regression.py
@@ -4,93 +4,149 @@ from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 
-from data_utils import (unify_date_format_year,
-                        clean_summary,
-                        safe_json_parse
-                        )
+from src.data_cleaning.data_utils import DataCleaner
 
 
-# Function to preprocess the data
-def preprocess_data(input_data):
-    # Clean the summary column
-    input_data['summary'] = input_data['summary'].apply(clean_summary)
+class DataPreprocessor:
+    """
+    A class to preprocess the input DataFrame.
 
-    # Parse JSON column to dictionary
-    input_data['freebase_id_json'] = input_data['freebase_id_json'].apply(safe_json_parse)
+    Attributes:
+        df (pandas.DataFrame): The input DataFrame.
 
-    # Extract genre IDs from JSON column
-    def extract_genre_ids(json_str):
-        return list(json_str.keys()) if isinstance(json_str, dict) else []
+    Methods:
+        preprocess_data(): Preprocesses the input data.
+    """
 
-    input_data['genre_ids'] = input_data['freebase_id_json'].apply(extract_genre_ids)
+    def __init__(self, df):
+        """
+        Initializes the DataPreprocessor object.
 
-    # Convert lists in 'genre_ids' column to strings
-    input_data['genre_ids'] = input_data['genre_ids'].apply(lambda x: ','.join(x) if isinstance(x, list) else x)
+        Args:
+            df (pandas.DataFrame): The input DataFrame.
+        """
+        self.df = df
 
-    # Ensure 'date' column remains in the DataFrame after cleaning
-    input_data['date'] = input_data['date'].fillna('')  # Fill missing values with ''
+    def preprocess_data(self):
+        """
+        Preprocesses the input data.
 
-    # Encode categorical features using OneHotEncoder
-    encoder = OneHotEncoder(handle_unknown='ignore')  # Handle unknown categories during encoding
-    encoded_features = encoder.fit_transform(input_data[['genre_ids']])
-    encoded_df = pd.DataFrame(encoded_features.toarray(), columns=encoder.get_feature_names_out(['genre_ids']))
+        Returns:
+            pandas.DataFrame: The cleaned and preprocessed DataFrame.
+        """
+        # Clean the summary column
+        self.df['summary'] = self.df['summary'].apply(DataCleaner.clean_summary)
 
-    # Concatenate the encoded DataFrame with the original DataFrame
-    cleaned_df = pd.concat([input_data, encoded_df], axis=1)
+        # Parse JSON column to dictionary
+        self.df['freebase_id_json'] = DataCleaner(df=self.df).parse_json_column('freebase_id_json')['freebase_id_json']
 
-    return cleaned_df
+        # Extract genre IDs from JSON column
+        self.df['genre_ids'] = self.df['freebase_id_json'].apply(
+            lambda x: list(x.keys()) if isinstance(x, dict) else [])
 
+        # Convert lists in 'genre_ids' column to strings
+        self.df['genre_ids'] = self.df['genre_ids'].apply(lambda x: ','.join(x) if isinstance(x, list) else x)
 
-# Function to train and predict based on genre
-def predict_year_based_on_genre(df):
-    # Clean and preprocess the data
-    df_cleaned = preprocess_data(df)
+        # Ensure 'date' column remains in the DataFrame after cleaning
+        self.df['date'] = self.df['date'].fillna('')  # Fill missing values with ''
 
-    # Extract features and target variable
-    X = df_cleaned.drop(columns=['date'])  # Assuming 'date' column is part of the features
-    y = df_cleaned['date']
+        # Encode categorical features using OneHotEncoder
+        encoder = OneHotEncoder(handle_unknown='ignore')
+        encoded_features = encoder.fit_transform(self.df[['genre_ids']])
+        encoded_df = pd.DataFrame(encoded_features.toarray(), columns=encoder.get_feature_names_out(['genre_ids']))
 
-    # Handle missing values in target variable
-    y = y.replace('', pd.NA).dropna().astype(int)
+        # Concatenate the encoded DataFrame with the original DataFrame
+        cleaned_df = pd.concat([self.df, encoded_df], axis=1)
 
-    # Encode genre_ids for training
-    encoder = OneHotEncoder(handle_unknown='ignore')
-    X_encoded = encoder.fit_transform(X[['genre_ids']])
-
-    # Filter X_encoded and y based on valid target values
-    valid_indices = y.dropna().index
-    X_encoded_filtered = X_encoded[valid_indices]
-    y_filtered = y.dropna()
-
-    # Split data for training and testing
-    X_train, X_test, y_train, y_test = train_test_split(X_encoded_filtered, y_filtered, test_size=0.2, random_state=42)
-
-    # Train your model and make predictions
-    model = LinearRegression()
-    model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
-
-    # Evaluate model performance
-    mse = mean_squared_error(y_test, y_pred)
-    print(f"Mean Squared Error: {mse}")
-
-    # Example prediction using genre information
-    new_genre_json = '{"1": "fantasy"}'  # Example JSON for a new genre
-    new_genre_ids = safe_json_parse(new_genre_json)
-    new_genre_ids_str = ','.join(new_genre_ids.keys())
-    new_genre_ids_bin = encoder.transform([[new_genre_ids_str]])  # Include a placeholder for the date
-    new_year_pred = model.predict(new_genre_ids_bin)
-    print(f"Predicted date for genre_ids {new_genre_ids}: {new_year_pred[0]}")
-
-    return model
+        return cleaned_df
 
 
-if __name__ == "__main__":
+class BookYearPredictor:
+    """
+    A class to predict the publication year of a book based on its genre.
+
+    Attributes:
+        df (pandas.DataFrame): The input DataFrame.
+        preprocessor (DataPreprocessor): The data preprocessor object.
+
+    Methods:
+        predict_year_based_on_genre(): Trains a linear regression model to predict the publication year based on genre.
+    """
+
+    def __init__(self, df):
+        """
+        Initializes the BookYearPredictor object.
+
+        Args:
+            df (pandas.DataFrame): The input DataFrame.
+        """
+        self.df = df
+        self.preprocessor = DataPreprocessor(df)
+
+    def predict_year_based_on_genre(self):
+        """
+        Trains a linear regression model to predict the publication year based on genre.
+
+        Returns:
+            sklearn.linear_model.LinearRegression: The trained linear regression model.
+        """
+        # Clean and preprocess the data
+        df_cleaned = self.preprocessor.preprocess_data()
+
+        # Extract features and target variable
+        X = df_cleaned.drop(columns=['date'])
+        y = df_cleaned['date']
+
+        # Handle missing values in target variable
+        y = y.replace('', pd.NA).dropna().astype(int)
+
+        # Encode genre_ids for training
+        encoder = OneHotEncoder(handle_unknown='ignore')
+        X_encoded = encoder.fit_transform(X[['genre_ids']])
+
+        # Filter X_encoded and y based on valid target values
+        valid_indices = y.dropna().index
+        X_encoded_filtered = X_encoded[valid_indices]
+        y_filtered = y.dropna()
+
+        # Split data for training and testing
+        X_train, X_test, y_train, y_test = train_test_split(X_encoded_filtered, y_filtered, test_size=0.2,
+                                                            random_state=42)
+
+        # Train the model and make predictions
+        model = LinearRegression()
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+
+        # Evaluate model performance
+        mse = mean_squared_error(y_test, y_pred)
+        print(f"Mean Squared Error: {mse}")
+
+        # Example prediction using genre information
+        new_genre_json = '{"1": "fantasy", "2": "adventure"}'
+        # new_genre_ids = self.preprocessor.safe_json_parse(new_genre_json)
+        new_genre_ids = DataCleaner(df=df_cleaned).parse_json_column(column_name='freebase_id_json')
+        new_genre_ids_str = ','.join(new_genre_ids.keys())
+        new_genre_ids_bin = encoder.transform([[new_genre_ids_str]])
+        new_year_pred = model.predict(new_genre_ids_bin)
+        print(f"Predicted date for genre_ids {new_genre_ids}: {new_year_pred[0]}")
+
+        return model
+
+
+def main():
     # Load data from text file into DataFrame
     column_names = ["length", "freebase_id", "book_name", "author_name", "date", "freebase_id_json", "summary"]
     data = pd.read_csv('../../data/datacolab_dataset/txt_format/booksummaries.txt', sep="\t", header=None,
                        names=column_names)
-    unify_date_format_year(data)
 
-    # Call the function with the loaded DataFrame
-    predict_year_based_on_genre(data)
+    # Unify the date format to year
+    data = DataCleaner(data).unify_date_format_year()
+
+    # Create an instance of the BookYearPredictor class and train the model
+    predictor = BookYearPredictor(data)
+    model = predictor.predict_year_based_on_genre()
+
+
+if __name__ == "__main__":
+    main()
